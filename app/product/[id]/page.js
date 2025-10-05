25536c1ad457dcf9278d0c9cf5a3c5dff52db426
shriramPraevia124 <shriram.tiwari@praevia.com>
2025-10-05 21:20:31 +0530

    updated

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -1,8 +1,27 @@
-import ProductDetailClient from './components/ProductDetailClient';
+"use server"
+
+import ProductDetail from './components/ProductDetail';
+import { fetchProductData, fetchPriceData } from './lib/fetchProduct';
 
 export default async function ProductDetailPage({ params }) {
   const resolvedParams = await params;
   const id = resolvedParams?.id;
 
-  return <ProductDetailClient productId={id} />;
+  if (!id) {
+    return <ProductDetail error="Product ID not found" />;
+  }
+
+  try {
+    const { product, productDescription } = await fetchProductData(id);
+    const priceData = await fetchPriceData(id);
+
+    return <ProductDetail 
+      product={product} 
+      productDescription={productDescription} 
+      priceData={priceData} 
+      productId={id} 
+    />;
+  } catch (err) {
+    return <ProductDetail error={err.message || 'Failed to load product'} />;
+  }
 }
