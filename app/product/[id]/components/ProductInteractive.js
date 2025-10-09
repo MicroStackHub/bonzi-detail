4812c875d1a23f616d898a30ec6c0847edbf8596
shriramPraevia124 <shriram.tiwari@praevia.com>
2025-10-10 00:56:49 +0530

    updated the color black error

--- app/product/[id]/components/ProductInteractive.js
+++ app/product/[id]/components/ProductInteractive.js
@@ -239,7 +239,7 @@ export default function ProductInteractive({ product, initialPriceData, productI
             {product.colors.map((color) => (
               <button 
                 key={color.id} 
-                className={`w-10 h-10 rounded border-2 flex items-center justify-center relative overflow-hidden ${
+                className={`w-10 h-10 rounded-md border-2 flex items-center justify-center relative overflow-hidden ${
                   selectedColor === color.id ? 'border-orange-500 ring-2 ring-orange-200' : 'border-gray-300 hover:border-orange-300'
                 }`}                
                 onClick={() => {
@@ -269,8 +269,9 @@ export default function ProductInteractive({ product, initialPriceData, productI
                   />
                 )}
                 {selectedColor === color.id && (
-                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-30 rounded-md">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-white" viewBox="0 0 20 20" fill="currentColor">
+                  // Small check badge in the top-right instead of darkening the whole image
+                  <div className="absolute top-1 right-1 w-6 h-6 rounded-full bg-white/95 flex items-center justify-center shadow">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 text-orange-500" viewBox="0 0 20 20" fill="currentColor">
                       <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                     </svg>
                   </div>
